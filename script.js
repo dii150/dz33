d44301db2cb15b5973a987ea806ab179d5e1d4ec
dii150 <diklimena@gmail.com>
2023-04-09 13:27:00 +0300

    commit!

---
+++ script.js
@@ -0,0 +1,19 @@
+$('.slider-container').slick({
+    dots: true,
+    autoplay: true,
+    arrows: false
+});
+
+$('.guitars-list').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    arrows: true
+});
+
+$('.partners-list').slick({
+    slidesToShow: 8,
+    infinite: true,
+    slidesToScroll: 3,
+    autoplay: true
+});
